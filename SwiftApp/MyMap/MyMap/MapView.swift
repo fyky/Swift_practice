918cb0203329971b335552d3e11cca125ce65ca4
fyky <fuyu12dmb25ms@gmail.com>
2022-01-27 17:03:28 +0900

    [Add]MapiKit中心ピン表示

--- SwiftApp/MyMap/MyMap/MapView.swift
+++ SwiftApp/MyMap/MyMap/MapView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 import MapKit
 
 struct MapView: UIViewRepresentable {
+    
+    let searchKey: String
+    
     func makeUIView(context: Context) -> MKMapView {
         
         MKMapView()
@@ -16,12 +19,43 @@ struct MapView: UIViewRepresentable {
     
     func updateUIView(_ uiView: MKMapView, context: Context) {
         
+        print(searchKey)
+        
+        let geocoder = CLGeocoder()
+        
+        geocoder.geocodeAddressString(
+            searchKey ,
+            completionHandler: {
+                (placemarks,error) in
+            
+            if let unwrapPlacemarks = placemarks ,
+               let firstPlacemark = unwrapPlacemarks.first,
+               let location = firstPlacemark.location {
+                let targetCoordinate = location.coordinate
+                
+                print(targetCoordinate)
+                
+                let pin = MKPointAnnotation()
+                
+                pin.coordinate = targetCoordinate
+                
+                pin.title = searchKey
+                
+                uiView.addAnnotation(pin)
+                
+                uiView.region = MKCoordinateRegion(
+                    center: targetCoordinate,
+                    latitudinalMeters: 500.0,
+                    longitudinalMeters: 500.0)
+            }
+            })
+        
     }
 
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(searchKey: "東京タワー")
     }
 }
