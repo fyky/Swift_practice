dca229b65f610b632ea94e069eacc63697f200eb
fyky <fuyu12dmb25ms@gmail.com>
2022-01-27 14:35:33 +0900

    [Add]じゃんけん

--- SwiftApp/MyJanken/MyJanken/ContentView.swift
+++ SwiftApp/MyJanken/MyJanken/ContentView.swift
@@ -8,17 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var answerNumber = 0
+    
     var body: some View {
         
         VStack {
-            Image("gu")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
             
-            Text("ぐー")
+            if answerNumber == 0 {
+                Text("これからじゃんけんします")
+            } else if answerNumber == 1{
+                Image("gu")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                Text("ぐー")
+            } else if answerNumber == 2{
+                Image("choki")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                Text("ちょき")
+            } else{
+                Image("pa")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                Text("ぱー")
+            }
             
             Button(action: {
-                print("タップされたよ")
+                answerNumber = Int.random(in: 1...3)
             }) {
                 Text("じゃんけんをする")
             }
